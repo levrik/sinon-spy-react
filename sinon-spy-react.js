51bf087da0d81f508a2d9a8581331773a6c8bacf
Antonio Pires <apires@users.noreply.github.com>
2016-05-04 17:30:28 -0500

    Update sinon-spy-react.js

--- sinon-spy-react.js
+++ sinon-spy-react.js
@@ -2,17 +2,50 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var sinon = require('sinon');
 
-function spyOnReactClass(reactClass, methodName) {
+function spyOnComponentMethod(reactClass, methodName) {
     var classProto = reactClassPrototype(reactClass);
-    var spy = sinon.spy();
+    var spy;
+    var on;
+    var idx;
 
     if (classProto.__reactAutoBindMap) { // React 0.14.x
-      classProto.__reactAutoBindMap[methodName] = spy;
+      spy = classProto.__reactAutoBindMap[methodName] = sinon.spy(classProto.__reactAutoBindMap[methodName]);
     } else if (classProto.__reactAutoBindPairs) { // React 15.x
-      classProto.__reactAutoBindPairs.push(methodName, spy);
+      idx = classProto.__reactAutoBindPairs.indexOf(methodName);
+      if(idx !== -1){
+          spy = classProto.__reactAutoBindPairs[idx+1] = sinon.spy(classProto.__reactAutoBindPairs[idx+1]);
+      } else {
+          throw new Error('Cannot spy on a function that does not exist');
+      }
     }
-
     return spy;
+}
+
+function mockComponentMethod(reactClass, methodName) {
+    var classProto = reactClassPrototype(reactClass);
+    var mock;
+    var on;
+    var idx;
+    var i;
+
+    if (classProto.__reactAutoBindMap) { // React 0.14.x
+      mock = classProto.__reactAutoBindMap[methodName] = sinon.mock(classProto.__reactAutoBindMap, methodName);
+    } else if (classProto.__reactAutoBindPairs) { // React 15.x
+      idx = classProto.__reactAutoBindPairs.indexOf(methodName);
+      if(idx !== -1){
+          mock = classProto.__reactAutoBindPairs[idx+1] = sinon.mock(classProto.__reactAutoBindPairs, idx+1);
+      } else {
+          mock = sinon.mock();
+          i = classProto.__reactAutoBindPairs.push( methodName );
+          classProto.__reactAutoBindPairs.push( methodName );
+          classProto.__reactAutoBindPairs.push( mock );
+          
+          mock.restore = function(){
+             classProto.__reactAutoBindPairs.splice(i-1, 2);   
+          }
+      }
+    }
+    return mock;
 }
 
 function reactClassPrototype(reactClass) {
@@ -23,4 +56,5 @@ function reactClassPrototype(reactClass) {
     return ctor.prototype;
 }
 
-module.exports = spyOnReactClass;
+module.exports.spyOnComponentMethod = spyOnComponentMethod;
+module.exports.mockComponentMethod = mockComponentMethod;
