6ac114df5055aa0700a86fab62bf3a0eee051fa8
Antonio Pires <apires@users.noreply.github.com>
2016-05-04 17:30:28 -0500

    Make spy code a bit more defensive and initial support for stubs

--- sinon-spy-react.js
+++ sinon-spy-react.js
@@ -2,17 +2,49 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var sinon = require('sinon');
 
-function spyOnReactClass(reactClass, methodName) {
+function spyOnComponentMethod(reactClass, methodName) {
     var classProto = reactClassPrototype(reactClass);
-    var spy = sinon.spy();
+    var spy;
+    var on;
+    var idx;
 
     if (classProto.__reactAutoBindMap) { // React 0.14.x
-      classProto.__reactAutoBindMap[methodName] = spy;
+      spy = classProto.__reactAutoBindMap[methodName] = sinon.spy(classProto.__reactAutoBindMap[methodName]);
     } else if (classProto.__reactAutoBindPairs) { // React 15.x
-      classProto.__reactAutoBindPairs.push(methodName, spy);
+      idx = classProto.__reactAutoBindPairs.indexOf(methodName);
+      if(idx !== -1){
+          spy = classProto.__reactAutoBindPairs[idx+1] = sinon.spy(classProto.__reactAutoBindPairs[idx+1]);
+      } else {
+          throw new Error('Cannot spy on a function that does not exist');
+      }
     }
-
     return spy;
+}
+
+function stubComponentMethod(reactClass, methodName) {
+    var classProto = reactClassPrototype(reactClass);
+    var stub;
+    var on;
+    var idx;
+    var i;
+
+    if (classProto.__reactAutoBindMap) { // React 0.14.x
+      stub = classProto.__reactAutoBindMap[methodName] = sinon.stub(classProto.__reactAutoBindMap, methodName);
+    } else if (classProto.__reactAutoBindPairs) { // React 15.x
+      idx = classProto.__reactAutoBindPairs.indexOf(methodName);
+      if(idx !== -1){
+          stub = sinon.stub(classProto.__reactAutoBindPairs, idx+1);
+      } else {
+          stub = sinon.stub();
+          i = classProto.__reactAutoBindPairs.push( methodName );
+          classProto.__reactAutoBindPairs.push( stub );
+
+          stub.restore = function(){
+             classProto.__reactAutoBindPairs.splice(i-1, 2);
+          }
+      }
+    }
+    return stub;
 }
 
 function reactClassPrototype(reactClass) {
@@ -23,4 +55,5 @@ function reactClassPrototype(reactClass) {
     return ctor.prototype;
 }
 
-module.exports = spyOnReactClass;
+module.exports.spyOnComponentMethod = spyOnComponentMethod;
+module.exports.stubComponentMethod = stubComponentMethod;
